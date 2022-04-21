e46b3bad88e49f29d8b494c89eea256659381e1b
Yasin-Yilmazz <yasinn.yilmazz26@gmail.com>
2022-04-22 00:23:56 +0300

    Last Update

--- in-class/JavaScript1/app.js
+++ in-class/JavaScript1/app.js
@@ -1,7 +1,39 @@
 // this code show the app is runnig
-console.log("***app.js***")
+// console.log("***app.js***")
 
-let ad = prompt("Enter yoru name")
+// let ad = prompt("Enter yoru name")
 
-console.log("adiniz: " + ad + " dir");
-console.log(`adiniz ${ad} dir`) // ters tırnak koyarak bu şekilde
+// console.log("adiniz: " + ad + " dir");
+// console.log(`adiniz ${ad} dir`) // ters tırnak koyarak bu şekilde
+
+
+var myNumber1 = 5;
+
+
+var myNumber2 = 8;
+console.log(myNumber2);
+
+let var1 = 55;
+console.log(var1);
+
+// var1 = 99;
+// console.log(var1); // tekrar atama yaparken hata veriyor
+{
+    let localVar1 = 'Local variable';
+    console.log(localVar1);
+}
+// console.log(localVar1); // let'le tanımladığımız için parantez dışında okuyamıyor
+
+
+// const konusu
+
+const student = {
+    firstname: 'Yasin',
+    number : 'F1375'
+}
+console.log(student);
+
+const otherStudent = student;
+console.log(otherStudent);
+otherStudent.firstname = 'Yılmaz'
+console.log(student);
