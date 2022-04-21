562eef96f0875ae7716a9b363cd3118a9f38c189
Yasin-Yilmazz <yasinn.yilmazz26@gmail.com>
2022-04-21 23:32:42 +0300

    last update

--- in-class/JavaScript1/app.js
+++ in-class/JavaScript1/app.js
@@ -1,2 +1,7 @@
 // this code show the app is runnig
 console.log("***app.js***")
+
+let ad = prompt("Enter yoru name")
+
+console.log("adiniz: " + ad + " dir");
+console.log(`adiniz ${ad} dir`) // ters tırnak koyarak bu şekilde
