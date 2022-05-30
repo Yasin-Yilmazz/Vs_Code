e77171dc3d3d2b4b615eb83c0f0955f2c92cc7a8
Yasin-Yilmazz <yasinnyilmazz26@gmail.com>
2022-05-30 13:39:12 +0300

    ss

---
+++ in-class/javascript8-Objects/js08_objects-newOperators/2-newOperators.js
@@ -0,0 +1,155 @@
+//* ======================================================
+//*    NEW GENERATION OPERATORS: DESTRUCTURING (OBJECT)
+//* ======================================================
+console.log("****** NEW OPERATORS *******");
+const car = {
+  name: "BMW",
+  model: 1990,
+  engine: 1.6,
+};
+
+//* 1.YONTEM (Classical)
+console.log(car.model);
+console.log(car["name"]);
+
+//* 2.YONTEM (Destructering)
+
+const { name, model, engine } = car;
+console.log(name, model, engine);
+
+//* EXAMPLE: NESTED
+const cars = {
+  car1: {
+    name: "BMW",
+    model: 1990,
+    engine: 1.6,
+  },
+  car2: {
+    name: "Mercedes",
+    model: 2022,
+    engine: 2.0,
+  },
+};
+
+const kisiler = [
+  {
+    name: "Ahmet",
+    surname: "Can",
+    job: "developer",
+    age: 30,
+  },
+  {
+    name: "Mehmet",
+    surname: "Baki",
+    job: "tester",
+    age: 35,
+  },
+  {
+    name: "Nur",
+    surname: "Ersan",
+    job: "team lead",
+    age: 40,
+  },
+  {
+    name: "Merve",
+    surname: "Veli",
+    job: "developer",
+    age: 26,
+  },
+
+  {
+    name: "Ruzgar",
+    surname: "Kuzey",
+    job: "tester",
+    age: 24,
+  },
+];
+
+kisiler.forEach((kisi) => {
+  console.log("NAME:", kisi.name);
+  console.log("SURNAME:", kisi.surname);
+  console.log("JOB:", kisi.job);
+  console.log("AGE:", kisi["age"]);
+  console.log("*************************");
+});
+
+kisiler.forEach((kisi) => {
+  console.log("**************************");
+  const { name, job, surname, age } = kisi;
+  console.log("NAME:", name);
+  console.log("SURNAME:", surname);
+  console.log("JOB:", job);
+  console.log("AGE:", age);
+});
+
+// function
+function getInfo() {
+  return {
+    id: 101,
+    name: "Apple",
+    price: 20000,
+  };
+}
+const { id, price } = getInfo();
+console.log(id, price);
+
+//* ======================================================
+//*    NEW GENERATION OPERATORS: DESTRUCTURING (ARRAYS)
+//* ======================================================
+
+const names = ["Ahmet", "Mehmet", "İsmet", "Saffet"];
+
+const name1 = names[0];
+const name2 = names[1];
+
+const [person1, person2, , person4] = names;
+console.log(person1, person2, person4);
+
+//* ======================================================
+//*    NEW GENERATION OPERATORS: SPREAD OR REST (...)
+//* ======================================================
+// REST: (ARRAYS)
+const vehicles = ["bmw", "reno", "mercedes", "ferrari", "anadol"];
+
+const [vec1, vec2, ...restVehicles] = vehicles;
+console.log(vec1, vec2);
+console.log(restVehicles);
+
+//* REST (Objects)
+
+const personel = {
+  pName: "John",
+  surname: "smith",
+  job: "developer",
+  age: 30,
+};
+
+const { pName, job, ...surnameAge } = personel;
+console.log(pName, job, surnameAge);
+
+//* REST (Function Arguments)
+const topla = (x, y) => {
+  return x + y;
+};
+
+//? hata vermez fakat saddece 2 argumanı toplar
+console.log(topla(1, 2, 3, 4, 5, 6));
+
+const hepsiniTopla = (...sayilar) => {
+  let toplam = 0;
+  console.log(sayilar);
+  sayilar.forEach((sayi) => (toplam += sayi));
+  return toplam;
+};
+
+console.log(hepsiniTopla(1, 2, 3, 4, 5, 6));
+
+//? -------------------------------------------------
+//? SPREAD
+//? -------------------------------------------------
+
+const araclar = ["Ucak", "Helikopter", "Bisiklet"];
+const otomobiller = ["Tır", "Otobus", "Araba", "SUV"];
+
+const tumAraclar = [...araclar, ...otomobiller];
+console.log(tumAraclar);
