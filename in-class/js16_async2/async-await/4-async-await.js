98781b5af0f041ae3d35cf693f6e8553eab5f742
Yasin-Yilmazz <yasinnyilmazz26@gmail.com>
2022-06-15 23:04:14 +0300

    ++

--- in-class/js16_async2/async-await/4-async-await.js
+++ in-class/js16_async2/async-await/4-async-await.js
@@ -17,3 +17,38 @@
 //* Await, promise-temelli herhangi bir fonksiyonun onune getirilerek getirildigi
 //* satirdaki kodun durudurulmasini saglar. Yapilan istek yerine getirilip sonuc
 //* degerlerinin dondurulmesine ile kodun calismasi devam eder.
+
+let err = false;
+const getUser = async () => {
+  try {
+    const res = await fetch("https://api.github.com/users");
+    if (!res.ok) {
+      err = true;
+      //   throw new Error(`Something went wrong:${res.status}`);
+    }
+
+    const data = await res.json();
+    updateDom(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getUser();
+
+const updateDom = (data) => {
+  const userDiv = document.querySelector(".users");
+
+  if (err) {
+    userDiv.innerHTML = `<h1 class="text-danger">Data can not be fetched</h1>`;
+  } else {
+    data.forEach((user) => {
+      const { login, avatar_url, html_url } = user;
+      userDiv.innerHTML += `
+<h2 class="text-danger">NAME: ${login}</h2>
+<img src="${avatar_url}" width = "50%" alt=""/>
+
+  `;
+    });
+  }
+};
--- in-class/js16_async2/flag-app/app.js
+++ in-class/js16_async2/flag-app/app.js
@@ -1,3 +1,73 @@
 //*=========================================================
 //*                     FLAG-APP
 //*=========================================================
+
+const fetchCountry = async (name) => {
+  const url = `https://restcountries.com/v3.1/name/${name}`;
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      renderError(`Something went wrong: ${res.status}`);
+      throw new Error();
+    }
+
+    const data = await res.json();
+    renderCountry(data[0]);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const renderError = (err) => {
+  const countriesDiv = document.querySelector(".countries");
+  countriesDiv.innerHTML = `
+  <h1 class="text-danger">${err}</h1>
+<img src="../img/150320221117049980314.png" alt="">
+  `;
+};
+
+const renderCountry = (country) => {
+  console.log(country);
+  const countriesDiv = document.querySelector(".countries");
+  const {
+    capital,
+    name: { common },
+    region,
+    flags: { svg },
+    languages,
+    currencies,
+  } = country;
+  //   const { name } = name.common;
+  //   console.log(capital[0], common, region, svg);
+  //   console.log(Object.values(languages));
+  //   console.log(Object.values(currencies[0].name))
+  //   console.log(Object.values(currencies[0].symbol))
+  countriesDiv.innerHTML += `
+
+    <div class="card shadow-lg" style="width: 18rem;">
+        <img src="${svg}" width = "50%" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${common}</h5>
+            <p class="card-text">${region}</p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item"><i class="fas fa-lg fa-landmark"></i> ${capital}</li>
+            <li class="list-group-item"><i class="fas fa-lg fa-comments"></i> ${Object.values(
+              languages
+            )}</li>
+            <li class="list-group-item"><i class="fas fa-lg fa-money-bill-wave"></i> ${
+              Object.values(currencies)[0].name
+            },${Object.values(currencies)[0].symbol}</li>
+        </ul>
+        <div class="card-body">
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+        </div>
+    </div>
+
+`;
+};
+
+fetchCountry("turkey");
+fetchCountry("usa");
+fetchCountry("france");
