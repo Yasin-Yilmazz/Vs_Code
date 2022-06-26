b4fdc2dd49b86fe7ebbe634a587a7c251a492989
Yasin-Yilmazz <yasinnyilmazz26@gmail.com>
2022-06-26 11:50:47 +0300

    q5

---
+++ coding-challenge/q5.js
@@ -0,0 +1,15 @@
+// Write a function that removes every lone 9 that is inbetween 7s.
+// "79712312" --> "7712312"
+// "79797"    --> "777"
+
+function sevenAte9(str) {
+  arr = str.split("");
+  for (let i = 1; i < arr.length - 1; i++) {
+    if (arr[i - 1] == "7" && arr[i] == "9" && arr[i + 1] == "7") {
+      arr.splice(i, 1);
+    }
+  }
+
+  return arr.join("");
+}
+sevenAte9("79712312");
