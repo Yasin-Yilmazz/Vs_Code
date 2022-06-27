0b89af3670568924bbf909cc1110a67dc31c8f4b
Yasin-Yilmazz <yasinnyilmazz26@gmail.com>
2022-06-27 23:05:48 +0300

    lesson end

--- React/rc05_events-useStateHook/src/App.js
+++ React/rc05_events-useStateHook/src/App.js
@@ -1,8 +1,13 @@
 import Event from "./components/1-event-intro/Event";
+import Counter from "./components/2-class-companents/Counter";
+import UseStateExample from "./components/3-useStateHook/UseStateExample";
 function App() {
   return (
     <div>
       <Event />
+      <Counter count={10} />
+      <Counter />
+      <UseStateExample />
     </div>
   );
 }
