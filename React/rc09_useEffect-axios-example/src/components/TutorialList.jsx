c7f7139eec31cf3aa75f89519bd720f69bb2ceab
Yasin-Yilmazz <yasinnyilmazz26@gmail.com>
2022-07-02 14:22:29 +0300

    ++

--- React/rc09_useEffect-axios-example/src/components/TutorialList.jsx
+++ React/rc09_useEffect-axios-example/src/components/TutorialList.jsx
@@ -1,20 +1,8 @@
-import { FaEdit } from 'react-icons/fa';
-import { AiFillDelete } from 'react-icons/ai';
+import { FaEdit } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
+import EditTutorial from "./EditTutorial";
 
-const TutorialList = () => {
-  //? Test data
-  let tutorials = [
-    {
-      id: 1,
-      title: 'ReactJS',
-      description: 'React is a JS-library for UI Design',
-    },
-    {
-      id: 2,
-      title: 'HTML',
-      description: 'HTML is a markup language',
-    },
-  ];
+const TutorialList = ({ tutorials, deleteTutorial, editTutorial }) => {
   return (
     <div className="container mt-4">
       <table className="table table-striped">
@@ -37,14 +25,26 @@ const TutorialList = () => {
                 <td>{title}</td>
                 <td>{description}</td>
                 <td className="text-center">
-                  <FaEdit size={20} className="me-3 text-warning " />
-                  <AiFillDelete size={22} className="text-danger" />
+                  <FaEdit
+                    data-bs-toggle="modal"
+                    data-bs-target="#edit-modal"
+                    size={20}
+                    className="me-3 text-warning cursor-pointer"
+                    onClick={() => editTutorial(id, "CSS", "Styleheet")}
+                  />
+                  <AiFillDelete
+                    size={22}
+                    className="text-danger cursor-pointer"
+                    onClick={() => deleteTutorial(id)}
+                  />
                 </td>
               </tr>
             );
           })}
         </tbody>
       </table>
+
+      <EditTutorial />
     </div>
   );
 };
