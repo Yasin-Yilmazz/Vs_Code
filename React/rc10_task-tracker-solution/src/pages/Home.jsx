a1ad16f1e6be29182814dcb72ad6699865962e89
Yasin-Yilmazz <yasinnyilmazz26@gmail.com>
2022-07-04 23:26:25 +0300

    task tracker ++

--- React/rc10_task-tracker-solution/src/pages/Home.jsx
+++ React/rc10_task-tracker-solution/src/pages/Home.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
+import Header from "../components/Header";
+import ShowTasks from "../components/ShowTasks";
+import { useState } from "react";
+import data from "../helper/starterData";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [tasks, setTasks] = useState(data);
+
+  console.log(tasks);
+  return (
+    <div>
+      <Header tasks={tasks} setTasks={setTasks} />
+      <ShowTasks />
+    </div>
+  );
 };
 
 export default Home;
