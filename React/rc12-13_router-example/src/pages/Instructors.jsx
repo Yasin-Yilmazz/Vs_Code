5b12e0231fe0afe0c393d36d8acab679d6a30ea7
Yasin-Yilmazz <yasinnyilmazz26@gmail.com>
2022-07-11 21:01:07 +0300

    ++

--- React/rc12-13_router-example/src/pages/Instructors.jsx
+++ React/rc12-13_router-example/src/pages/Instructors.jsx
@@ -1,12 +1,13 @@
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Instructors = () => {
-  const [instructors, setInstructors] = useState([]);
   const navigate = useNavigate();
 
+  const [instructors, setInstructors] = useState([]);
+
   const getInstructors = () => {
-    fetch("https://jsonplaceholder.typicode.com/users")
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then((res) => res.json())
       .then((data) => setInstructors(data))
       .catch((err) => console.log(err));
@@ -25,12 +26,16 @@ const Instructors = () => {
             <div
               className="col-sm-12 col-md-6 col-lg-4"
               key={id}
-              onClick={() => navigate(`/instructors/${id}`, { inst })}
+              // onClick={() => navigate(`/instructors/${id}`)}
+              //! Relative path
+              onClick={() => navigate(`${id}`, { state: inst })}
+              //? Absolute path
+              // onClick={() => navigate(`/instructors/${id}`, { state: inst })}
+              style={{ cursor: 'pointer' }}
             >
               <img
                 src={`https://avatars.dicebear.com/v2/avataaars/${id}.svg`}
                 alt=""
-                role="button"
               />
               <h6>{name}</h6>
             </div>
